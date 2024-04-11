67fa6d68e52719c2226674274c9a48399c8e5025
Aonani <Lightkarlton@gmail.com>
2024-04-11 13:04:24 +0100

    add await keyword

--- lib/features/home/presentation/view/home_page.dart
+++ lib/features/home/presentation/view/home_page.dart
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
       body: RefreshIndicator(
         color: Colors.black,
         onRefresh: () async {
-          homeViewModel.refresh();
+         await homeViewModel.refresh();
         },
         child: ListView(
           padding: const EdgeInsets.only(left: 8.0),
